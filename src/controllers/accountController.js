8c1f0942753d34f00775b9508bb6b2fa5cd3fad5
Sergio Moyano <sergio.moyano@outlook.com.ar>
2024-09-11 09:33:23 -0300

    fix

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -2,53 +2,53 @@ const accountService = require("../services/accountService");
 
 const getAllAccounts = (req, res) => {
     accountService.getAllAccounts()
-    .then((data) => {
-        res.status(200).json({ data: data });       
-    })
-    .catch((error) => {
-        res.status(200).json({ data: error });        
-    })
-    
+        .then((data) => {
+            res.status(200).json({ data: data });
+        })
+        .catch((error) => {
+            res.status(200).json({ data: error });
+        })
+
 };
 
 const getAccountById = (req, res) => {
     accountService.getAccountById(req.params.accountId)
-    .then((data) => {
-        res.status(200).json({ data: data });  
-    })
-    .catch((error) => {
-        res.status(200).json({ data: error });
-    });
+        .then((data) => {
+            res.status(200).json({ data: data });
+        })
+        .catch((error) => {
+            res.status(200).json({ data: error });
+        });
 };
 
 const insertAccount = (req, res) => {
     accountService.insertAccount(req.body)
-    .then((data) => {
-        res.status(201).json({ data: data });  
-    })
-    .catch((error) => {
-        res.status(200).json({ data: error });
-    });
+        .then((data) => {
+            res.status(201).json({ data: data });
+        })
+        .catch((error) => {
+            res.status(200).json({ data: error });
+        });
 };
 
 const updateAccount = (req, res) => {
     accountService.updateAccount(req.body, req.params.accountId)
-    .then((data) => {
-        res.status(201).json({ data: data });  
-    })
-    .catch((error) => {
-        res.status(200).json({ data: error });
-    })
+        .then((data) => {
+            res.status(201).json({ data: data });
+        })
+        .catch((error) => {
+            res.status(200).json({ data: error });
+        })
 };
 
 const deleteAccount = (req, res) => {
     accountService.deleteAccount(req.params.accountId)
-    .then((data) => {
-        res.status(200).json({data: data});
-    })
-    .catch((error) => {
-        res.status(200).json({ data: error });
-    })
+        .then((data) => {
+            res.status(200).json({ data: data });
+        })
+        .catch((error) => {
+            res.status(200).json({ data: error });
+        })
 };
 
 module.exports = {
--- src/services/accountService.js
+++ src/services/accountService.js
@@ -1,47 +1,47 @@
 const modelAccounts = require("./../database/models").accounts;
 
 const getAllAccounts = () => {
-    return modelAccounts.findAll({raw: true});
+  return modelAccounts.findAll({ raw: true });
 };
 
 const getAccountById = (accountId) => {
-    return modelAccounts.findByPk(accountId);
+  return modelAccounts.findByPk(accountId);
 
 };
 
 const insertAccount = (account) => {
-    return modelAccounts.create(account);
+  return modelAccounts.create(account);
 };
 
 const updateAccount = (account, accountId) => {
-    return modelAccounts.update(
-        account,
-        {
-          where: {
-            id: accountId,
-          },
-        },
-      );
+  return modelAccounts.update(
+    account,
+    {
+      where: {
+        id: accountId,
+      },
+    },
+  );
 };
 
 const deleteAccount = (accountId) => {
-    //logical delete 
-    return modelAccounts.update(
-        {
-            'status': 'DL'
-        },
-        {
-          where: {
-            id: accountId,
-          },
-        },
-      );
+  //logical delete 
+  return modelAccounts.update(
+    {
+      'status': 'DL'
+    },
+    {
+      where: {
+        id: accountId,
+      },
+    },
+  );
 }
 
 module.exports = {
-    getAllAccounts,
-    getAccountById,
-    insertAccount,
-    updateAccount,
-    deleteAccount
+  getAllAccounts,
+  getAccountById,
+  insertAccount,
+  updateAccount,
+  deleteAccount
 };
